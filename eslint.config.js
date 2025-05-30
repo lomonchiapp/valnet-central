b3df046eb250404c85270dbbf7ca348d804d67d6
lomonchiapp <lomonchiapp@gmail.com>
2025-05-30 00:35:00 -0400

    toy pueto

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,12 @@ import reactHooks from 'eslint-plugin-react-hooks'
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+  },
   react.configs.recommended,
   reactHooks.configs.recommended,
   {
